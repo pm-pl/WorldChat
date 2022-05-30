80343e062c7699fe382d234ccd6d4963ce633aef
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-05-30 19:36:30 +0700

    Typo

--- src/WorldChat/EventListener.php
+++ src/WorldChat/EventListener.php
@@ -21,7 +21,7 @@ class EventListener implements Listener {
 	 */
 	public function onChat(PlayerChatEvent $event){
 		$player = $event->getPlayer();
-		if($this->plugin->isChatDisabled($player->getWorld ()->getDisplayName())) {
+		if($this->plugin->isChatDisabled($player->getWorld()->getDisplayName())) {
 		    if($this->plugin->cfg["log-chat-disabled"]){
 		        $player->sendMessage(TextFormat::colorize(WorldChat::PREFIX . "&cChat disabled on this world"));
 		    }
