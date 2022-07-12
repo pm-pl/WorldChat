d57651599283b5fd2a28e97e5d3b6820ae86aa17
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-07-12 11:52:25 +0700

    ??

--- src/WorldChat/EventListener.php
+++ src/WorldChat/EventListener.php
@@ -17,7 +17,7 @@ class EventListener implements Listener {
 		$player = $event->getPlayer();
 		if($this->plugin->isChatDisabled($player->getWorld()->getDisplayName())) {
 		    if($this->plugin->cfg["log-chat-disabled"]){
-		        $player->sendMessage(TextFormat::colorize(WorldChat::PREFIX . "&cChat disabled on this world"));
+		        $player->sendMessage(TextFormat::colorize(WorldChat::$prefix . "&cChat disabled on this world"));
 		    }
 		    $event->cancel();
 		}
