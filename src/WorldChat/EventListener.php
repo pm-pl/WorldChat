80250d35c5bacd7df00db3818d1070379d152166
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-17 20:23:19 +0700

    Use getFolderName()

--- src/WorldChat/EventListener.php
+++ src/WorldChat/EventListener.php
@@ -15,7 +15,7 @@ class EventListener implements Listener {
 	
 	public function onChat(PlayerChatEvent $event){
 		$player = $event->getPlayer();
-		if($this->plugin->isChatDisabled($player->getWorld()->getDisplayName())) {
+		if($this->plugin->isChatDisabled($player->getWorld()->getFolderName())) {
 		    if($this->plugin->cfg["log-chat-disabled"]){
 		        $player->sendMessage(TextFormat::colorize($this->plugin->getConfig()->get("prefix") . "&cChat disabled on this world"));
 		    }
