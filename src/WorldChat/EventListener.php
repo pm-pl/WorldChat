8cf9ac58e4dcdc7e60181c29669b5f0b1ab04f49
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-18 19:34:34 +0700

    Formatiing #1

--- src/WorldChat/EventListener.php
+++ src/WorldChat/EventListener.php
@@ -9,26 +9,26 @@ use pocketmine\utils\TextFormat;
 
 class EventListener implements Listener {
     
-	public function __construct(private WorldChat $plugin){
-		$this->plugin = $plugin;
-	}
+    public function __construct(private WorldChat $plugin) {
+	    $this->plugin = $plugin;
+    }
 	
-	public function onChat(PlayerChatEvent $event){
-		$player = $event->getPlayer();
-		if($this->plugin->isChatDisabled($player->getWorld()->getFolderName())) {
-		    if($this->plugin->cfg["log-chat-disabled"]){
-		        $player->sendMessage(TextFormat::colorize($this->plugin->getConfig()->get("prefix") . "&cChat disabled on this world"));
-		    }
-		    $event->cancel();
-		}
-		$recipients = $event->getRecipients();
-		foreach($recipients as $key => $recipient){
-			if($recipient instanceof Player){
-				if($recipient->getWorld() != $player->getWorld()){
-					unset($recipients[$key]);
-				}
-			}
-		}
-		$event->setRecipients($recipients);
+    public function onChat(PlayerChatEvent $event) {
+	$player = $event->getPlayer();
+	if($this->plugin->isChatDisabled($player->getWorld()->getFolderName())) {
+	    if($this->plugin->cfg["log-chat-disabled"]) {
+                $player->sendMessage(TextFormat::colorize($this->plugin->getConfig()->get("prefix") . "&cChat disabled on this world"));
+	    }
+	    $event->cancel();
 	}
+	$recipients = $event->getRecipients();
+	foreach($recipients as $key => $recipient) {
+            if($recipient instanceof Player) {
+	        if($recipient->getWorld() != $player->getWorld()) {
+		    unset($recipients[$key]);
+		}
+	    }
+	}
+	$event->setRecipients($recipients);
+    }
 }
